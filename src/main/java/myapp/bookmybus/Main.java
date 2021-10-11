a06d183b4d35af149b9bceca0dd39aa20fa3bf03
MathanKumar14 <mathankumar2k2@gmail.com>
2021-10-11 15:31:51 +0530

    Second commit

--- src/main/java/myapp/bookmybus/Main.java
+++ src/main/java/myapp/bookmybus/Main.java
@@ -11,8 +11,8 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("login.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load(), 700, 500);
+        stage.setTitle("BookMyBus");
         stage.setScene(scene);
         stage.show();
     }
