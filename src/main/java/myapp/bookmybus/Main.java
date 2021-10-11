56241f37c5547973a58e17a347e88f556705e748
MathanKumar14 <mathankumar2k2@gmail.com>
2021-10-11 15:34:53 +0530

    Title changed

--- src/main/java/myapp/bookmybus/Main.java
+++ src/main/java/myapp/bookmybus/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("login.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 700, 500);
-        stage.setTitle("_BookMyBus_");
+        stage.setTitle("BookMyBus");
         stage.setScene(scene);
         stage.show();
     }
